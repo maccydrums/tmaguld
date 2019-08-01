a5596c46091bf307ea963f4525cd5642e729d161
maccydrums <marcusprivat@live.com>
2019-08-01 17:25:12 +0200

    prod

---
+++ build/precache-manifest.bffafa570b85befde25d8acf98272478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a4d832c4f1edb4cae051a29a5cc6bd5",
+    "url": "/tmaguld/static/media/top.1a4d832c.jpg"
+  },
+  {
+    "revision": "369bc7914ef6b78fa596",
+    "url": "/tmaguld/static/js/runtime~main.ff57814e.js"
+  },
+  {
+    "revision": "77969d92c0ec7a79e929",
+    "url": "/tmaguld/static/js/main.735ac061.chunk.js"
+  },
+  {
+    "revision": "7584b5a3d184c7aa178b",
+    "url": "/tmaguld/static/js/2.78e4ce6a.chunk.js"
+  },
+  {
+    "revision": "77969d92c0ec7a79e929",
+    "url": "/tmaguld/static/css/main.2596da52.chunk.css"
+  },
+  {
+    "revision": "14aac3b1af17edaeebbd002c7aba801d",
+    "url": "/tmaguld/index.html"
+  }
+];
